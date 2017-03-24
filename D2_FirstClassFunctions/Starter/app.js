06167468456ac8a632d47fc12f38b13a60f99017
Sean M Hamlet <seanmhamlet@gmail.com>
2017-03-24 15:09:49 -0400

    updates with first-class functions and function expressions

--- D2_FirstClassFunctions/Starter/app.js
+++ D2_FirstClassFunctions/Starter/app.js
@@ -1,1 +1,26 @@
-// Your Javascript Code Goes Here
+// function statement
+function greet() {
+  console.log('hi');
+}
+greet();
+
+// functions are first-class
+function logGreeting(fn) {
+  fn();
+}
+logGreeting(greet);
+
+// function expression (anonymous function because of no-name as in above two)
+var greetMe = function() {
+  console.log('Hi Sean');
+};
+greetMe();
+
+// it's still first-class
+logGreeting(greetMe);
+
+// use a function expression on the fly
+
+logGreeting(function() {
+  console.log('Hello Sean!');
+})
