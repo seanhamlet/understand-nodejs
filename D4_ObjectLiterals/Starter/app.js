e5f4ddee5bfb9208ffaf6e4f3b9d8efd727132e9
Sean M Hamlet <seanmhamlet@gmail.com>
2017-03-25 06:36:16 -0400

    Adds object literal code

--- D4_ObjectLiterals/Starter/app.js
+++ D4_ObjectLiterals/Starter/app.js
@@ -1,1 +1,12 @@
-// Your Javascript Code Goes Here
+var person = {
+  firstname: 'John',
+  lastname: 'Doe',
+  greet: function() {
+    console.log('Hello ' + this.firstname + ' ' + this.lastname)
+  }
+};
+
+// access object values using dot
+person.greet();
+// or bracket notation
+console.log(person['firstname']);
