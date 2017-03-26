1b5689f010594ee3869890ba35ecb767f56cef1c
Sean M Hamlet <seanmhamlet@gmail.com>
2017-03-26 19:30:06 -0400

    Adds notes on require examples

---
+++ D10_MoreOnRequire/Starter/greet/index.js
@@ -0,0 +1,8 @@
+var english = require('./english');
+var spanish = require('./spanish');
+
+// export two functions by exporting an object
+module.exports = {
+  english: english,
+  spanish: spanish
+};
