9ad93cd6e36312ddcaf6c53959c53fdd2d89f9c8
Sean M Hamlet <seanmhamlet@gmail.com>
2017-03-26 13:01:20 -0400

    Adds notes on IIFE

--- D7_IIFE/Starter/app.js
+++ D7_IIFE/Starter/app.js
@@ -1,1 +1,13 @@
-// Your Javascript Code Goes Here
+var firstname = 'Jane';
+
+// first set of parentheses around function make it a function expression instead
+// of a function statements
+(function (lastname) {
+  var firstname = 'John';
+  console.log(firstname);
+  console.log(lastname);
+}('Doe'));
+// the reason firstname is not affected by 'john' is because the variables
+// are protected inside the function
+
+console.log(firstname);
