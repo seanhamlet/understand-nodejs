a1087635128569ec031af79a781f4a2304f9bb0d
Sean M Hamlet <seanmhamlet@gmail.com>
2017-04-04 08:41:35 -0400

    Adds more notes on pipes and streams

--- F11_Pipes/Starter/app.js
+++ F11_Pipes/Starter/app.js
@@ -1,10 +1,26 @@
 var fs = require('fs');
+var zlib = require('zlib');
 
-var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 16 * 1024 });
+// readable stream
+var readable = fs.createReadStream(__dirname + '/greet.txt');
 
+//writable stream
 var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
 
-readable.on('data', function(chunk) {
-	console.log(chunk);
-	writable.write(chunk);
-});
+var compressed = fs.createWriteStream(__dirname + '/greet.txt.gz');
+
+// creates transform stream (readable and writable)
+// every time chunk is sent to it, it compresses that chunk
+// transforms data into compressed version
+var gzip = zlib.createGzip();
+
+// readable is source and writable is destination
+// pipe sets up event listener to listen for chunk of data
+readable.pipe(writable);
+
+// can keep using .pipe if destination returned is readable and writable (e.g. duplex or transform stream)
+
+// can stream file that anything that is a stream (e.g. file or internet connection)
+
+// read from readable, compress with gzip, and write to compressed file
+readable.pipe(gzip).pipe(compressed);
