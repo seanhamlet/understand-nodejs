f1b235423552de621f3f64d6cb6a808eed949b2d
Sean M Hamlet <seanmhamlet@gmail.com>
2017-04-06 13:52:12 -0400

    Adds notes on routing

--- G10_Routing/Starter/app.js
+++ G10_Routing/Starter/app.js
@@ -3,11 +3,24 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
 
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  var obj = {
-    firstname: 'John',
-    lastname: 'Doe'
-  };
-  res.end(JSON.stringify(obj));
+  if (req.url === '/') {
+    fs.createReadStream(__dirname + '/index.htm').pipe(res);
+  }
 
-}).listen(8080, '127.0.0.1');
+  else if (req.url === '/api'){
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var obj = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+    res.end(JSON.stringify(obj));
+  }
+
+  else {
+    // if file not found return 404 error
+    res.writeHead(404);
+    // then just send nothing
+    res.end();
+  }
+
+}).listen(1337, '127.0.0.1');
