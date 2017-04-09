93b5bda1ba8ce434bcb0ac8243c2cbb8fbc7dbee
Sean M Hamlet <seanmhamlet@gmail.com>
2017-04-08 21:21:47 -0400

    Adds notes on mysql

--- J2_NodeAndMySQL/Starter/app.js
+++ J2_NodeAndMySQL/Starter/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
@@ -12,6 +13,22 @@ app.set('view engine', 'ejs');
 
 app.use('/', function (req, res, next) {
 	console.log('Request Url:' + req.url);
+
+	// connect to mysql database
+	var con = mysql.createConnection({
+		host: "localhost",
+		user: "test",
+		password: "test",
+		database: "addressbook"
+	});
+
+	// run a query to get data from database
+	con.query('SELECT People.ID, Firstname, Lastname, Address FROM People INNER JOIN PersonAddresses ON People.ID = PersonAddresses.PersonID INNER JOIN Addresses ON PersonAddresses.AddressID = Addresses.ID', 
+		function(err, rows) {
+			if (err) throw err;
+			console.log(rows);
+		});
+
 	next();
 });
 
